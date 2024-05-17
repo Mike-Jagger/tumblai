6e039cd4cf74f6048c76f1fd6c64f92054e0f7cc
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-05-17 14:07:27 -0600

    Removed dom update listener and timer

--- public/background.js
+++ public/background.js
@@ -1,30 +1,14 @@
 /* eslint-disable no-undef */
-var tabID;
-
 chrome.runtime.onInstalled.addListener(() => { // Fire on install or update
     chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         if (changeInfo.status === 'complete' && tab.url && tab.url.includes('tumblr.com')) {
             tabID = tabId;
-            console.log('New Page Opened')
-            setTimeout (() => {
-                const isLoaded = true;
-                chrome.tabs.sendMessage(tabId, {
-                    type: "NEW",
-                    pageLoaded: isLoaded,
-                });
-            }, 1500); // Wait a few seconds for complete DOM to load
-            
+            console.log('New Page Opened');
+            const isLoaded = true;
+            chrome.tabs.sendMessage(tabId, {
+                type: "NEW",
+                pageLoaded: isLoaded,
+            })   
         }
     });
 });
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'UPDATE') {
-        console.log('DOM updated');
-        const isLoaded = true;
-        chrome.tabs.sendMessage(tabID, {
-            type: "NEW",
-            pageLoaded: isLoaded,
-        });
-    }
-  });
