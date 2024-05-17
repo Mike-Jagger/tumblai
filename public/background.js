a60ac3c80965c29b401b03906b73d069bc21c243
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-05-16 19:26:19 -0600

    Added method to fire the extension on update or install

--- public/background.js
+++ public/background.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-undef */
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.url && tab.url.includes('tumblr.com')) {
-      console.log("Test");
-      const isLoaded = true;
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        pageLoaded: isLoaded,
-      });
-    }
-  });
-  
+chrome.runtime.onInstalled.addListener(() => { // Fire on install or update
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+        if (changeInfo.status === 'complete' && tab.url && tab.url.includes('tumblr.com')) {
+        console.log("Test");
+        const isLoaded = true;
+        chrome.tabs.sendMessage(tabId, {
+            type: "NEW",
+            pageLoaded: isLoaded,
+        });
+        }
+    });
+});
