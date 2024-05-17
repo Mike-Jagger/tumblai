9761fc8b962ec6982978eeadec34250796461df6
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-05-17 12:05:49 -0600

    Completing new design

--- src/components/CommentComponent.jsx
+++ src/components/CommentComponent.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
+import "./CommentComponent.scss";
 
 function CommentComponent(props) {
   const [tone, setTone] = useState('Friendly');
@@ -27,82 +28,17 @@ function CommentComponent(props) {
     }
   };
 
-  const commentSectionStyle = {
-    width: "112px",
-    height: "75px",
-    display: "flex",
-    flexDirection: "column",
-    justifyIontent: "center",
-    alignItems: "center",
-    backgroundColor: "#52565e",
-    fontSize: "5px",
-    borderRadius: "11%",
-    marginLeft: "5px",
-    color: "black"
-  }
-
-  const selectToneStyle = {
-    fontSize: "8px",
-    width: "100%",
-    height: "12px",
-    border: "solid 1px #52565e"
-  }
-
-  const optionsStyle = {
-    fontSize: "8px"
-  }
-
-  const textareaStyle = {
-    fontSize: "8px",
-    width: "95%",
-    minWidth: "95%",
-    maxWidth: "95%",
-    height: "100%",
-    maxHeight: "100%",
-    placeHolder: "Click load to Generate"
-  }
-
-  const buttonSectionStyle = {
-    width: "100%",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-around"
-  }
-
-  const buttonStyleLoad = {
-    padding: 0,
-    fontSize: "8px",
-    textAlign: "center",
-    height: "9px",
-    width: "30%",
-    border: "solid 0px",
-    borderRadius: "20%",
-    backgroundColor: "#FFC845"
-  }
-
-  const buttonStylePost = {
-    padding: 0,
-    fontSize: "8px",
-    textAlign: "center",
-    height: "9px",
-    width: "30%",
-    border: "solid 0px",
-    borderRadius: "20%",
-    backgroundColor: "#C1FF72"
-  }
-
   return (
-    <div style={commentSectionStyle}>
-      <select value={tone} onChange={(e) => setTone(e.target.value)} style={selectToneStyle}>
-        <option value="Friendly" style={optionsStyle}>Friendly</option>
-        <option value="Funny" style={optionsStyle}>Funny</option>
-        <option value="Disagree" style={optionsStyle}>Disagree</option>
+    <div className="comment-section">
+      <select value={tone} onChange={(e) => setTone(e.target.value)} className="select-tone">
+        <option value="Friendly">---&emsp;FRIENDLY&emsp;---</option>
+        <option value="Funny">---&emsp;FUNNY&emsp;---</option>
+        <option value="Disagree">---&emsp;DISAGREE&emsp;---</option>
         {/* Add more tones as needed */}
       </select>
-      <textarea value={comment} style={textareaStyle} placeholder='Click load to generate comment' readOnly/>
-      <div style={buttonSectionStyle}>
-        <button onClick={handleGenerateComment} style={buttonStyleLoad}>Load</button>
-        <button onClick={handlePostComment} style={buttonStylePost}>Post</button>
+      <div className="button-section">
+        <button onClick={handleGenerateComment} className="button-load">Load</button>
+        <button onClick={handlePostComment} className="button-post">Post</button>
       </div>
     </div>
   );
