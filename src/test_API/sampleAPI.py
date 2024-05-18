1e7ea1cbd32bce8d56c5dd0d09617172e9dc2ec9
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-05-18 14:43:44 -0600

    enabling CORS for all routes

--- src/test_API/sampleAPI.py
+++ src/test_API/sampleAPI.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Load the comments from the JSON file
 with open('TestComments.json', 'r') as f:
