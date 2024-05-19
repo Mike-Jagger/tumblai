ad7c0a02f9be24272a4b63caba57f49dde33757f
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-05-18 19:07:39 -0600

    Renaming and refactoring files

--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App.js';
+import './popup.scss';
+import PopUpWindow from './components/PopUpWindow.jsx';
 import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <PopUpWindow />
   </React.StrictMode>
 );
 
--- src/popup.test.js
+++ src/popup.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import PopUpWindow from './components/PopUpWindow';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Load/i);
+  render(<PopUpWindow />);
+  const linkElement = screen.getByText(/ON/i);
   expect(linkElement).toBeInTheDocument();
 });
